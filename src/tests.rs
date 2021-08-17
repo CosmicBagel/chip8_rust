b398a62aefeb222191f2a9192b925a49b49aa372
Samuel Whiteley <sam@cosmicbagel.com>
2021-08-16 20:42:54 -0600

    Wrote the last few tests

--- src/tests.rs
+++ src/tests.rs
@@ -280,3 +280,143 @@ fn the_0x9XY0_test() {
     emu.execute_instruction(0x9010.into());
     assert!(emu.program_counter == 0x202);
 }
+
+#[test]
+fn the_0xANNN_test() {
+    let mut emu = Emulator::new_headless();
+    assert!(emu.address_register == 0x0);
+    emu.execute_instruction(0xA250.into());
+    assert!(emu.address_register == 0x250);
+}
+
+#[test]
+fn the_0xBNNN_test() {
+    let mut emu = Emulator::new_headless();
+    assert!(emu.program_counter == 0x200);
+    emu.registers[0] = 0x0;
+
+    emu.execute_instruction(0xB250.into());
+    assert!(emu.program_counter == 0x250);
+
+    let mut emu = Emulator::new_headless();
+    assert!(emu.program_counter == 0x200);
+    emu.registers[0] = 0x3;
+
+    emu.execute_instruction(0xB250.into());
+    assert!(emu.program_counter == 0x253);
+}
+
+#[test]
+fn the_0xCXNN_test() {
+    //test rng (can sometimes fail)
+    let mut emu = Emulator::new_headless();
+    assert!(emu.registers[0] == 0x0);
+    emu.execute_instruction(0xC0FF.into());
+    assert!(emu.registers[0] != 0);
+
+    //test mask
+    let mut emu = Emulator::new_headless();
+    assert!(emu.registers[0] == 0x0);
+    emu.execute_instruction(0xC0AA.into());
+    assert!(emu.registers[0] & 0x55 == 0);
+}
+
+#[test]
+fn the_0xFX07_test() {
+    let mut emu = Emulator::new_headless();
+    emu.timer_counter = 30;
+
+    emu.registers[3] = 0;
+    emu.execute_instruction(0xF307.into());
+    assert!(emu.registers[3] == 30);
+}
+
+#[test]
+fn the_0xFX15_test() {
+    let mut emu = Emulator::new_headless();
+    assert!(emu.timer_counter == 0);
+
+    emu.registers[3] = 30;
+    emu.execute_instruction(0xF315.into());
+    assert!(emu.timer_counter == 30);
+}
+
+#[test]
+fn the_0xFX18_test() {
+    let mut emu = Emulator::new_headless();
+    assert!(emu.sound_counter == 0);
+
+    emu.registers[3] = 30;
+    emu.execute_instruction(0xF318.into());
+    assert!(emu.sound_counter == 30);
+}
+
+#[test]
+fn the_0xFX1E_test() {
+    // typical add
+    let mut emu = Emulator::new_headless();
+    assert!(emu.address_register == 0);
+    emu.registers[5] = 0xFF;
+    emu.execute_instruction(0xF51E.into());
+    assert!(emu.address_register == 0xFF);
+
+    //overflowing add
+    let mut emu = Emulator::new_headless();
+    emu.address_register = 0xFFFF;
+    emu.registers[5] = 0x1;
+    emu.execute_instruction(0xF51E.into());
+    assert!(emu.address_register == 0x0);
+}
+
+#[test]
+fn the_0xFX33_test() {
+    let mut emu = Emulator::new_headless();
+    emu.address_register = 0x205;
+    emu.registers[3] = 223;
+    emu.execute_instruction(0xF333.into());
+    assert!(emu.memory_space[0x205] == 2);
+    assert!(emu.memory_space[0x206] == 2);
+    assert!(emu.memory_space[0x207] == 3);
+
+    let mut emu = Emulator::new_headless();
+    emu.address_register = 0x205;
+    emu.registers[3] = 13;
+    emu.execute_instruction(0xF333.into());
+    assert!(emu.memory_space[0x205] == 0);
+    assert!(emu.memory_space[0x206] == 1);
+    assert!(emu.memory_space[0x207] == 3);
+
+    let mut emu = Emulator::new_headless();
+    emu.address_register = 0x205;
+    emu.registers[3] = 5;
+    emu.execute_instruction(0xF333.into());
+    assert!(emu.memory_space[0x205] == 0);
+    assert!(emu.memory_space[0x206] == 0);
+    assert!(emu.memory_space[0x207] == 5);
+}
+
+#[test]
+fn the_fx55_test() {
+    let mut emu = Emulator::new_headless();
+    emu.address_register = 0x500;
+    for i in 0..0xF {
+        emu.registers[i] = i as u8;
+    }
+    emu.execute_instruction(0xFF55.into());
+    for i in 0..0xF {
+        assert!(emu.memory_space[0x500 + i] == i as u8);
+    }
+}
+
+#[test]
+fn the_fx65_test() {
+    let mut emu = Emulator::new_headless();
+    emu.address_register = 0x500;
+    for i in 0..0xF {
+        emu.memory_space[0x500 + i] = i as u8;
+    }
+    emu.execute_instruction(0xFF65.into());
+    for i in 0..0xF {
+        assert!(emu.registers[i] == i as u8);
+    }
+}
