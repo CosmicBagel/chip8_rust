0cf2cb40f44e4a22a93252e280b6fe8d572e33a8
Samuel Whiteley <sam@cosmicbagel.com>
2021-08-16 18:33:32 -0600

    Added more tests

--- src/tests.rs
+++ src/tests.rs
@@ -2,6 +2,7 @@ use super::*;
 
 #[test]
 fn jump_test() {
+    // tests 0x1NNN
     let mut emu = Emulator::new_headless();
     let result = emu.execute_instruction(0x1200.into());
 
@@ -15,6 +16,7 @@ fn jump_test() {
 
 #[test]
 fn subroutine_test() {
+    // tests 0x2NNN and 0x00EE
     let mut emu = Emulator::new_headless();
     let result = emu.execute_instruction(0x2500.into());
 
@@ -51,4 +53,88 @@ fn subroutine_test() {
 
     assert!(emu.program_counter == 0x202);
     assert!(emu.subroutine_return_pointers.is_empty());
+}
+
+#[test]
+fn the_0x3XNN_test() {
+    let mut emu = Emulator::new_headless();
+    emu.registers[2] = 0xFF;
+    emu.execute_instruction(0x3215.into());
+
+    assert!(emu.program_counter == 0x202);
+
+    let mut emu = Emulator::new_headless();
+    emu.registers[2] = 0x15;
+    emu.execute_instruction(0x3215.into());
+
+    assert!(emu.program_counter == 0x204);
+}
+
+#[test]
+fn the_0x4XNN_test() {
+    let mut emu = Emulator::new_headless();
+    emu.registers[2] = 0xFF;
+    emu.execute_instruction(0x4215.into());
+
+    assert!(emu.program_counter == 0x204);
+
+    let mut emu = Emulator::new_headless();
+    emu.registers[2] = 0x15;
+    emu.execute_instruction(0x4215.into());
+
+    assert!(emu.program_counter == 0x202);
+}
+
+#[test]
+fn the_0x5XY0_test() {
+    let mut emu = Emulator::new_headless();
+    emu.registers[1] = 0x00;
+    emu.registers[2] = 0xFF;
+    emu.execute_instruction(0x5120.into());
+
+    assert!(emu.program_counter == 0x202);
+
+    let mut emu = Emulator::new_headless();
+    emu.registers[1] = 0x15;
+    emu.registers[2] = 0x15;
+    emu.execute_instruction(0x5120.into());
+
+    assert!(emu.program_counter == 0x204);
+}
+
+#[test]
+fn the_0x6XNN_test() {
+    let mut emu = Emulator::new_headless();
+    emu.execute_instruction(0x60FF.into());
+    assert!(emu.registers[0] == 0xFF);
+}
+
+#[test]
+fn the_0x7XNN_test() {
+    let mut emu = Emulator::new_headless();
+
+    emu.execute_instruction(0x7001.into());
+    assert!(emu.registers[0] == 0x1);
+
+    emu.execute_instruction(0x70FF.into());
+    assert!(emu.registers[0] == 0x0);
+}
+
+#[test]
+fn the_0x8XY0_test() {
+    let mut emu = Emulator::new_headless();
+    emu.registers[0] = 0xFF;
+
+    emu.execute_instruction(0x8100.into());
+    assert!(emu.registers[1] == 0xFF);
+}
+
+#[test]
+fn the_0x8XY1_test() {
+    let mut emu = Emulator::new_headless();
+    emu.registers[0] = 0x0E;
+    emu.registers[1] = 0xF0;
+
+    emu.execute_instruction(0x8101.into());
+    assert!(emu.registers[1] == 0xFE);
 }
